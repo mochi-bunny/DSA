cb663ba8080401b2e00da15d6f088c404f303e48
mochi-bunny <99592507+mochi-bunny@users.noreply.github.com>
2022-04-05 17:33:38 +0400

    Update dsa.java

--- Code/dsa.java
+++ Code/dsa.java
@@ -1,11 +1,115 @@
 package Code; // this file is in a subfolder of the git repo
 
 // packages
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+public class Link //BINARYSEARCH
+{
+    // instance variables - replace the example below with your own
+    private Node head;
+    private Node tail;
+    private int size;
+    
+      Link(){
+        this.head=new Node("");
+        this.tail=new Node("");
+        this.size=0;
+    }
+    
+    void push(String e){
+        Node n = new Node (e);
+        Node x = this.tail.getPrev ();
+        if (x != null){
+    	x.setNext (n);
+    	n.setPrev (x);
+          }
+        else{
+    	    this.head.setNext (n);
+          }
+          
+        this.tail.setPrev (n);
+        this.size++;
+      }
+     
+    void display(){
+         
+             System.out.println("list:");
+            Node x= this.head.getNext();
+            while(x!=null){
+                System.out.println(x.getVal()+" ");
+                x=x.getNext();
+            }
+        }
+        
+    String traverse(int mid){
+        Node x=this.head.getNext();
+        int i=0;
+        while(x!=null && i<mid){
+        x=x.getNext();
+        i++;
+        }
+        return x.getVal();
+    }
+    void binsearch(String e){
+        int l=0; int f=0;
+        int r=this.size;
+        Node x= this.head.getNext();
+        
+        StringTokenizer skz= new StringTokenizer(e, " ");
+        int ele= Integer.parseInt(skz.nextToken());
+        
+        while(l<=r){
+            int mid=(l+r)/2;
+            String b= traverse(mid);
+            StringTokenizer stz= new StringTokenizer(b, " ");
+            int item= Integer.parseInt(stz.nextToken());
+            
+            if (ele==item){ f=1; break;}
+            else if ( ele<item ) r=mid-1;
+            else l=mid+1;
+        }
+        
+        if (f==1) System.out.println("rec found");
+	else  System.out.println("rec not found");
+    }
+        
+    }
+
+class Node{
+    private Node prev;
+    private Node next;
+    private String val;
+    
+    public Node(String a){
+        this.val=a;
+        this.next=null;
+        this.prev=null;
+    }
+    
+    void setNext(Node n){
+        this.next=n;
+    }
+    
+    Node getNext(){
+        return this.next;
+    }
+    
+    void setPrev(Node n){
+        this.prev=n;
+    }
+    
+    Node getPrev(){
+        return this.prev;
+    }
+    
+    String getVal(){
+        return this.val;
+    }
+    
+}
+ 
 class Stack
 {
     int t=-1;
