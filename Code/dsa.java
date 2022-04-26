65c1af3010586f86243636f180c1b0202af3a011
mochi-bunny <99592507+mochi-bunny@users.noreply.github.com>
2022-04-26 18:56:27 +0400

    Update dsa.java

--- Code/dsa.java
+++ Code/dsa.java
@@ -1,206 +1,169 @@
 package Code; // this file is in a subfolder of the git repo
 
-// packages
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-public class Link //BINARYSEARCH
-{
-    // instance variables - replace the example below with your own
-    private Node head;
-    private Node tail;
-    private int size;
-    
-      Link(){
-        this.head=new Node("");
-        this.tail=new Node("");
-        this.size=0;
-    }
-    
-    void push(String e){
-        Node n = new Node (e);
-        Node x = this.tail.getPrev ();
-        if (x != null){
-    	x.setNext (n);
-    	n.setPrev (x);
-          }
-        else{
-    	    this.head.setNext (n);
-          }
-          
-        this.tail.setPrev (n);
-        this.size++;
-      }
-     
-    void display(){
-         
-             System.out.println("list:");
-            Node x= this.head.getNext();
-            while(x!=null){
-                System.out.println(x.getVal()+" ");
-                x=x.getNext();
-            }
+class Record{   //STORE RECORDS
+        int id;
+        String name;
+        int age;
+        String role;
+        String position;
+        
+        public Record(){
+            String x= "empty";
         }
         
-    String traverse(int mid){
-        Node x=this.head.getNext();
-        int i=0;
-        while(x!=null && i<mid){
-        x=x.getNext();
-        i++;
-        }
-        return x.getVal();
-    }
-    void binsearch(String e){
-        int l=0; int f=0;
-        int r=this.size;
-        Node x= this.head.getNext();
-        
-        StringTokenizer skz= new StringTokenizer(e, " ");
-        int ele= Integer.parseInt(skz.nextToken());
-        
-        while(l<=r){
-            int mid=(l+r)/2;
-            String b= traverse(mid);
-            StringTokenizer stz= new StringTokenizer(b, " ");
-            int item= Integer.parseInt(stz.nextToken());
+        public Record(String rec){
+            StringTokenizer st= new StringTokenizer(rec, ",");
+            this.id= Integer.parseInt(st.nextToken());
+            this.name= st.nextToken();
+            this.age= Integer.parseInt(st.nextToken());
+            this.role= st.nextToken();
+            this.position=st.nextToken();
+             //print();
             
-            if (ele==item){ f=1; break;}
-            else if ( ele<item ) r=mid-1;
-            else l=mid+1;
         }
         
-        if (f==1) System.out.println("rec found");
-	else  System.out.println("rec not found");
-    }
+        void print(){
+            System.out.println("\nid: "+id+"\nname: "+name+"\nage: "+age+"\nrole: "+role+"\nposition: "+position);
+            
+        }
         
-    }
-
-class Node{
-    private Node prev;
-    private Node next;
-    private String val;
-    
-    public Node(String a){
-        this.val=a;
-        this.next=null;
-        this.prev=null;
-    }
-    
-    void setNext(Node n){
-        this.next=n;
-    }
-    
-    Node getNext(){
-        return this.next;
-    }
-    
-    void setPrev(Node n){
-        this.prev=n;
-    }
-    
-    Node getPrev(){
-        return this.prev;
-    }
-    
-    String getVal(){
-        return this.val;
-    }
-    
 }
- 
-class Stack
-{
-    int t=-1;
-    String[] stack;
+
+class Queue{
+    
     int size=0;
-    
-    public Stack(int n){
-        size=n;
-        stack= new String[size];
-    }
-    
-    
-    void push(String a){
-        if (t==size-1) System.out.println("stack full");
-        else{
-            t=t+1;
-            stack[t]=a;
-            System.out.println("VALUE INSERTED");
-        }
-    }
-    
-    void pop(){
-        if (t==-1) System.out.println("STACK EMPTY");
-        else{
-            String s=stack[t];
-            stack[t]=null;
-            t=t-1;
-            System.out.println(s+" removed from STACK");
+    int last=0;
+    class Stack
+    {
+        int t=-1;
+        Record[] stack;
+        //int size=0;
+        
+        public Stack(){
             
+            stack= new Record[size];
+            last=size;
         }
-    }
-    
-     void display(){
-        for(int i=0; i<size;i++){
-            if (stack[i]!=null) System.out.println(stack[i]);
+        
+        boolean isEmpty(){
+            if(t==-1) return true;
+            return false;
         }
-    }   
-}
-
-class Queue
-{
-      int f=0;
-      int r=0;
-      int size;
-      
-      String[] q;
-      
-      public Queue(int n){
-          size=n;
-          q= new String[size];
-      }
-      
-      
-      
-      void enqueue(String d){
-            if (r==(size-1))    System.out.println("FULL Q");
-            else{
-                q[r]=d;
-                r=r+1;
-                System.out.println("inserted");
+        
+        boolean isFull(){
+            if (t==size-1){
+                
+                return true;
+                
+                
             }
-     }
-     
-     void display(){
-        for(int i=0; i<size;i++){
-            if (q[i]!=null) System.out.println(q[i]);
-
+            return false;
         }
+        void push(Record a){
+            
+            if (t==size-1) System.out.println("stack full");
+            else{
+                
+                t=t+1;
+                stack[t]=a;
+                System.out.println("VALUE INSERTED");
+            }
+        
+        }
+        
+        Record pop(){
+            Record s= new Record();
+           if(!isEmpty()){
+                 s=stack[t];
+                stack[t]=null;
+                t=t-1;
+                
+                //System.out.println(s+" removed from STACK");
+            }    
+            
+            return s;
+        }
+        
+        void display(){
+            if(!isEmpty()){
+            for(int i=0; i<size;i++){
+                if (stack[i]!=null) stack[i].print();
+    
+            }
+                
+          }
+        }
+        
+        
     }
     
-    void dequeue(){
-        if (f==r){ System.out.println("EMPTY QUEUE");  }
+    Stack s1; Stack s2;
+    
+    void copy(Stack stack1, Stack stack2){
+       
+        for (int i=0;i<size ;i++ ){
+            stack2.push(stack1.stack[i]);
+        } 
+        //stack2.display();
+    }
+    
+    public Queue(int n){
+        size= n;
+        s1= new Stack();
+        s2= new Stack();
+    }
+    
+    void enqueue(Record rec){
+        s1.push(rec); //CREATING ENQUEUE STACK
         
-        else{
-            String de=q[f];
-            q[f]=null;
-            f=f+1;
-            System.out.println(de+" removed");
+        if (s1.isFull()){
+          Stack s3= new Stack();  //DUMMY STACK FOR POPPING VALUES FROM ENQUEUE AND INSERTING INTO DEQUEUE
+          
+           copy(s1,s3);
+           System.out.println("FULL");
+           
+           
+            while(!s3.isEmpty()){  //CREATING DEQUEUE STACK
+               Record r1= s3.pop();
+               s2.push(r1);
+           }
+           
+           
         }
+    
         
-    }   
-        
+    }
 }
 
 
-class dsa
+public class Main
 {
-	static String inputFile = "input.txt";
-	static String outputFile = "output.txt";
-	public static void main(String[] args) throws FileNotFoundException
-	{
-		System.out.println("Hello World");
+	public static void main(String[] args) {
+		   try{ 
+	        File f= new File("sports.txt");
+	        Scanner sc= new Scanner(f,"utf-8");
+	        
+	        Queue stck1= new Queue(3);  //1st stack
+	       
+	        
+	        while(sc.hasNextLine()){
+	            
+	        String line= sc.nextLine();
+	        Record recn= new Record(line);
+	         stck1.enqueue(recn);
+	         }
+	         
+	        System.out.println("enqueue stack");
+	          stck1.s1.display();
+             System.out.println("\n\ndequeue stack");
+             stck1.s2.display();
+	          
+	       
+	   }catch(IOException e){ e.printStackTrace(); }
+	    
 	}
 }
